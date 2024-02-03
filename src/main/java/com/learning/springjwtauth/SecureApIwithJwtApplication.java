3616ff687afbbb5aa91c5ec6bb39b7dfebae5cfc
Syed Basit Yar <basityar6@gmail.com>
2024-02-04 00:13:47 +0530

    initial commit

---
+++ src/main/java/com/learning/springjwtauth/SecureApIwithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.learning.springjwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApIwithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApIwithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/springjwtauth/SecureApIwithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.springjwtauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApIwithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
