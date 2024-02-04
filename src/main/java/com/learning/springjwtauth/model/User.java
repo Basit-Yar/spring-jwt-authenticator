7bb6f3168a94bdbcbb23aa1492ee50df24787f60
Syed Basit Yar <basityar6@gmail.com>
2024-02-04 14:24:58 +0530

    implementing userDetails and add roles.

--- src/main/java/com/learning/springjwtauth/model/User.java
+++ src/main/java/com/learning/springjwtauth/model/User.java
@@ -5,12 +5,18 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Document
-public class User {
+public class User implements UserDetails {
 
     @Id
     private String userId;
@@ -19,4 +25,33 @@ public class User {
     private String password;
     private Role roles;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(roles.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
